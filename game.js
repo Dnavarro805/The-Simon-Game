ddbf2dc20c580b9d3a44e9aaaf3493ea9c17d3c6
Dnavarro88 <42564706+Dnavarro88@users.noreply.github.com>
2021-05-31 16:01:35 -0700

    Added animations and sounds to user clicks

--- game.js
+++ game.js
@@ -1,1 +1,38 @@
-alert("Hello There!");
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+
+    playSound(userChosenColor);
+
+    animatePress(userChosenColor);
+})
+
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    // Add the new random chosen color to the end of the game pattern
+    gamePattern.push(randomChosenColor);
+
+    $select(randomChosenColor);
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
